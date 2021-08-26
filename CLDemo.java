746cfeb95298257bf909dae7141b9c442d188335
aligator51 <89272663+aligator51@users.noreply.github.com>
2021-08-26 16:33:18 +0300

    Update CLDemo.java

--- CLDemo.java
+++ CLDemo.java
@@ -1,1 +1,14 @@
+public class CLDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Была передана комманда длинной в " + args.length + " аргумента(ов)");
+
+		System.out.println("Аргументы:");
+		for(int i = 0; i < args.length; i ++) {
+			System.out.println("arg[" + i + "]: " + args[i]);
+		}
+	}
+
+}
 
